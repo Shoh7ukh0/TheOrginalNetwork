cc6524b24dfd3367a02fb08f26b5dc9a78f47752
Shoh7ukh0 <avalondokoni@gmail.com>
2023-11-06 10:18:25 +0500

    four commit

--- account/authentication.py
+++ account/authentication.py
@@ -13,8 +13,8 @@ def authenticate(self, request, username=None, password=None):
     except (User.DoesNotExist, User.MultipleObjectsReturned):
         return None
         
-    def get_user(self, user_id):
-        try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
-            return None
+def get_user(self, user_id):
+    try:
+        return User.objects.get(pk=user_id)
+    except User.DoesNotExist:
+        return None

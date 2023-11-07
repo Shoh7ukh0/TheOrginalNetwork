63e6ccf619cb6a108e95219681faac95985fe814
Shoh7ukh0 <avalondokoni@gmail.com>
2023-11-08 00:29:51 +0500

    images qo'shildi

--- Config/settings.py
+++ Config/settings.py
@@ -178,3 +178,17 @@ SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '316288742019-2o89ajpufmh02a4qu52ot36pr6ijg23t.apps.googleusercontent.com' # ИД клиента Google
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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' # Секрет клиента Google
+
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]

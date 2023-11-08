fe97c3204a40ee326dfc8a45e1ad7ac03127ec4c
Shoh7ukh0 <avalondokoni@gmail.com>
2023-11-08 21:07:53 +0500

    Post quyish boshlandi

--- original/urls.py
+++ original/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
+
+app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index)
+    # post ko‘rilgan
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
+    path('tag/<slug:tag_slug>/',views.post_list, name='post_list_by_tag'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('search/', views.post_search, name='post_search'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('', views.post_list, name='post_list'),
 ]
-

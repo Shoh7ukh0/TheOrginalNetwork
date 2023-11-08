7a093f6da5b5860231db3fcf7bbad13aa2567001
Shoh7ukh0 <avalondokoni@gmail.com>
2023-11-08 21:46:19 +0500

    Post tug'irlanyapti

--- original/models.py
+++ original/models.py
@@ -19,6 +19,7 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    images = models.ImageField(upload_to='original/post/images')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)

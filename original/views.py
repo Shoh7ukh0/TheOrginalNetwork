55fcc5b064bfd5217d63df3bc8306b485d5e1e3e
Shoh7ukh0 <avalondokoni@gmail.com>
2023-11-19 22:14:17 +0500

    DeletePostView sozlanyapti

--- original/views.py
+++ original/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import DeleteView
+from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.views import View
 from django.shortcuts import get_object_or_404

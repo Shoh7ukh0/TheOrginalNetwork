cb9c79c4c49c3fed6504e76709edfaf3c823a861
Shoh7ukh0 <avalondokoni@gmail.com>
2023-11-19 21:27:38 +0500

    password sozlanyapti

--- account/forms.py
+++ account/forms.py
@@ -5,7 +5,8 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control',
                                                                 'placeholder': 'Enter the username'}))
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control fakepassword',
+                                                                'placeholder': 'Enter the password'}))
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password', widget=forms.PasswordInput)

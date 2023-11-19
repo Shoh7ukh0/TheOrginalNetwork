2e79edabfc9bc6a141f7e275d95914f553aefe36
Shoh7ukh0 <avalondokoni@gmail.com>
2023-11-20 02:30:40 +0500

    STATICFILESDIRS aded

--- Config/settings.py
+++ Config/settings.py
@@ -143,9 +143,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
-)
+STATICFILESDIRS = BASE_DIR / 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'

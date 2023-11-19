f4c76c584e4572eabb41f9c42b493bab2a6b78d7
Shoh7ukh0 <avalondokoni@gmail.com>
2023-11-19 21:30:35 +0500

    UserEditForm widgets yozildi formlarni sozlanyapti

--- account/forms.py
+++ account/forms.py
@@ -55,6 +55,22 @@ class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
+
+        widgets = {
+            'first_name': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                }),
+            'last_name': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                }),
+            'email': forms.EmailInput(
+                attrs={
+                    'class': 'form-control',
+                }
+            )
+        }
     
     def clean_email(self):
         data = self.cleaned_data['email']

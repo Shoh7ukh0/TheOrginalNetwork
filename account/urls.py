7c563a1dbb616f6553d8575d1fab45e6d28bf711
Shoh7ukh0 <avalondokoni@gmail.com>
2023-11-19 21:25:09 +0500

    urls sozlanyapti

--- account/urls.py
+++ account/urls.py
@@ -5,7 +5,7 @@ app_name = 'account'
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
-    path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
+    path('dashboard/<str:username>/', views.ProfileView.as_view(), name='dashboard'),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('edit-profile/', views.EditProfileView.as_view(), name='edit_profile'),
     path('user-list/', views.UserListView.as_view(), name='user_list'),

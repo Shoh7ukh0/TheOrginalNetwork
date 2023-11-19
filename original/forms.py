c42580ba74effab69469bf276df3eb58bc8cc070
Shoh7ukh0 <avalondokoni@gmail.com>
2023-11-19 21:00:13 +0500

    forms.py yangilandi

--- original/forms.py
+++ original/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Post, Comment
+from django.forms.widgets import ClearableFileInput
 
 class SearchForm(forms.Form):
     q = forms.CharField(label='Search', max_length=100)
@@ -8,9 +9,39 @@ class SearchForm(forms.Form):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['caption', 'image', 'video']
+        fields = ['caption', 'hashtag', 'image', 'video']
+    
+        widgets = {
+            'caption': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Add a Post...'
+                }),
+            'hashtag': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Add a hashtag...'
+                }),
+            'image': ClearableFileInput(
+                attrs={
+                    'class': 'dropzone dropzone-default card shadow-none'
+                }),
+            'video': ClearableFileInput(
+                attrs={
+                    'class': 'dropzone dropzone-default card shadow-none'
+                }
+            )
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['text']
+
+        widgets = {
+            'text': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Add a Comment...'
+                }),
+        }

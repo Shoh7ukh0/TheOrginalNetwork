2bf9d7443043821a304fd15df8ee956645ab9e6c
Shoh7ukh0 <avalondokoni@gmail.com>
2023-12-04 12:17:39 +0500

    Davom etyapti

--- chat/routing.py
+++ chat/routing.py
@@ -1,7 +1,7 @@
-from django.urls import re_path
-from . import consumers
+from core import consumers
+
+from django.conf.urls import url
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/room/(?P<course_id>\d+)/$', 
-            consumers.ChatConsumer.as_asgi()),
+    url(r'^ws$', consumers.ChatConsumer),
 ]
--- chat/urls.py
+++ chat/urls.py
@@ -1,11 +1,16 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from rest_framework.routers import DefaultRouter
+from core.api import MessageModelViewSet, UserModelViewSet
 
-
-app_name = 'chat'
-
+router = DefaultRouter()
+router.register(r'message', MessageModelViewSet, basename='message-api')
+router.register(r'user', UserModelViewSet, basename='user-api')
 
 urlpatterns = [
-    path('room/<int:course_id>/', views.course_chat_room,
-         name='course_chat_room'),
+    path(r'api/v1/', include(router.urls)),
+
+    path('', login_required(
+        TemplateView.as_view(template_name='core/chat.html')), name='home'),
 ]

fb94257b4001a2acb2013e2f347566f9b07d1e5c
Shoh7ukh0 <avalondokoni@gmail.com>
2023-12-04 12:09:15 +0500

    consumer yangilandi

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,19 +1,46 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        # ulanishni qabul qilish
-        self.accept()
-    
-    def disconnect(self, close_code):
-        pass
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        user_id = self.scope['session']['_auth_user_id']
+        self.group_name = '{}'.format(user_id)
 
-    # WebSocket dan xabar olish
-    def receive(self, text_data):
+        # Join room Group
+
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
-        messages = text_data_json['message']
+        message = text_data_json['message']
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.chat_group_name,
+            {
+                'type': 'recieve_group_message',
+                'message':message
+            }
+        )  
 
-        # WebSocket-ga xabar yuborish
-        
-        self.send(text_data=json.dumps({'message':message}))
+    async def recieve_group_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        await self.send(
+            text_data=json.dumps({
+                'message': message
+            })
+        )

5e6d7b75650ff9a14c55837d472fcffb48cdc658
Shoh7ukh0 <avalondokoni@gmail.com>
2023-12-05 20:03:20 +0500

    Xato tuzatildi. Chatning keyingi bosqichiga o'tim

--- chat/routing.py
+++ chat/routing.py
@@ -1,7 +1,7 @@
 from . import consumers
 
-from django.conf.urls import url
+from django.urls import include, re_path
 
 websocket_urlpatterns = [
-    url(r'^ws$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'^ws$', consumers.ChatConsumer.as_asgi()),
 ]

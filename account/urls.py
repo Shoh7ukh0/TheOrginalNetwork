ab9c234446f965c110516f31ca647a7b4ab3d714
Shoh7ukh0 <avalondokoni@gmail.com>
2023-12-24 13:10:13 +0500

    Password tiklashlar tuliq yakunlandi

--- account/urls.py
+++ account/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
+    # path('', include('django.contrib.auth.urls')),
     path('', views.LoginView.as_view(), name='login'),
     path('dashboard/<str:username>/', views.ProfileView.as_view(), name='dashboard'),
     path('register/', views.RegistrationView.as_view(), name='register'),
@@ -24,12 +24,10 @@ urlpatterns = [
     path('save_post/<slug:slug>/', views.save_post, name='save_post'),
     path('delete_saved_post/<slug:slug>/', views.delete_saved_post, name='delete_saved_post'),
 
-    # path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     # # url-адреса смены пароля
-    # path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    # path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-    # # url-адреса сброса пароля
-    # path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    # path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done')
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # url-адреса сброса пароля
+    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
